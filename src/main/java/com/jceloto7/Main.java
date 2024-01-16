5a7a75ff42205e1cbb97b3aff8b9e659a1c25007
jceloto7 <jessicaceloto@gmail.com>
2024-01-16 17:24:29 -0300

    Finished

--- src/main/java/com/jceloto7/Main.java
+++ src/main/java/com/jceloto7/Main.java
@@ -1,7 +1,34 @@
 package com.jceloto7;
 
+import java.util.concurrent.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numberOfThreads = 5;
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            final int threadNumber = i;
+            executorService.execute(() -> {
+                long startTime = System.currentTimeMillis();
+                performTimeConsumingOperation();
+                long endTime = System.currentTimeMillis();
+                long elapsedTime = endTime - startTime;
+                System.out.println("Thread " + threadNumber + " took " + elapsedTime + " milliseconds");
+            });
+        }
+
+        executorService.shutdown();
+    }
+
+    private static void performTimeConsumingOperation() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 1000000; i++) {
+            result.append("a");
+        }
     }
 }
+
+
+
+
